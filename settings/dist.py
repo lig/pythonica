302a3c0a8d76586e3382adaa1786146a051804a9
Serge Matveenko <s@matveenko.ru>
2009-06-05 17:13:18 +0400

    Fix root urls module name

--- settings/dist.py
+++ settings/dist.py
@@ -91,7 +91,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
-ROOT_URLCONF = 'pythonica.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
