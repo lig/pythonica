ad77e3fe40aa39cef4a350185d262e0f42ded1e2
Serge Matveenko <s@matveenko.ru>
2010-04-21 14:56:30 +0400

    Explain sinals import in core.__init__.

--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,20 @@
-import signals
+"""
+Copyright 2009-2010 Serge Matveenko
+
+This file is part of Pythonica.
+
+Pythonica is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Pythonica is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Pythonica.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import signals # to enable signals at all
